4a3f3103c253a5c5d73a6ac30c5bcfdb0fe56245
arnoob16 <arnab4srk@gmail.com>
2020-09-20 14:13:16 +0530

    made mcq

--- vimanazGibberish.js
+++ vimanazGibberish.js
@@ -1,101 +1,101 @@
 var questions = {
-    1 : {
+    1: {
         question: "All tie, dude!",
         answer: "altitude",
         hint: "When you reach higher levels of it, you have difficulty in breathing."
     },
     2: {
-        question:"Missed isle",
+        question: "Missed isle",
         answer: "Missile",
         hint: "A Deadly weapon projected at a target"
     },
     3: {
-        question:"Rack it",
+        question: "Rack it",
         answer: "rocket",
         hint: "A jet engine containing its own propellant"
     },
     4: {
-        question:"Pro pulse shunt",
+        question: "Pro pulse shunt",
         answer: "Propulsion",
         hint: "The act of setting in motion"
     },
     5: {
-        question:"Tart Bull Lens",
+        question: "Tart Bull Lens",
         answer: "Turbulence",
         hint: "Instability in the Atmosphere."
     },
     6: {
-        question:"Pie Load",
+        question: "Pie Load",
         answer: "Pilot",
         hint: "a person licensed to operate an aircraft"
     },
     7: {
-        question:"Navy gate shunt",
+        question: "Navy gate shunt",
         answer: "Navigation",
         hint: " Guidance of ships/airplanes from place to place."
     },
     8: {
-        question:"Tick of",
+        question: "Tick of",
         answer: "Take-off",
         hint: "head for somewhere else"
     },
     9: {
-        question:"Eer odd anime icks",
+        question: "Eer odd anime icks",
         answer: "Aerodynamics",
         hint: "drag reduction"
     },
     10: {
-        question:"Yee Haw",
+        question: "Yee Haw",
         answer: "Yaw",
         hint: "oscillation about a vertical axis"
     },
     11: {
-        question:"fich",
+        question: "fich",
         answer: "pitch",
         hint: "A motion on an aircraft's lateral axis"
     },
     12: {
-        question:"Hair Yeal",
+        question: "Hair Yeal",
         answer: "Aerial",
         hint: "operating in the air."
     },
     13: {
-        question:"Raw Oil",
+        question: "Raw Oil",
         answer: "Roll",
         hint: "rotation about an axis"
     },
     14: {
-        question:"Throw Tile",
+        question: "Throw Tile",
         answer: "Throttle",
         hint: "device controlling flow of fuel"
     },
     15: {
-        question:"eer bois",
+        question: "eer bois",
         answer: "airbus",
         hint: "commerical aircraft"
     },
     16: {
-        question:"Fusy Laj",
+        question: "Fusy Laj",
         answer: "FuseLage",
         hint: "the main body of an aircraft."
     },
     17: {
-        question:"C Latte",
+        question: "C Latte",
         answer: "Slat",
         hint: "You don't need hint for this one"
     },
     18: {
-        question:"Fill App",
+        question: "Fill App",
         answer: "Flap",
         hint: "movement of wings"
     },
     19: {
-        question:"Elle Ruin",
+        question: "Elle Ruin",
         answer: "Aileron",
         hint: " flight control surface"
     },
     20: {
-        question:"Stay label rise",
+        question: "Stay label rise",
         answer: "Stabilizer",
         hint: "aerodynamic surface"
     }
@@ -103,70 +103,270 @@ var questions = {
 
 var mcqQues = {
     1: {
-        question : "Why lorem ipsum dolor sit amet?",
-        answer: "lmao NOOB",
+        question: "In Europe, drones up to 25kg (55lb) can be operated upto which maximum height?",
+        answer: "394ft",
         options: {
-            a: "lmao",
-            b: "noob",
-            c: "NOOB",
-            d: "lmao NOOB"
+            a: "394ft",
+            b: "4996ft",
+            c: "14393ft",
+            d: "31989ft",
+        }
+    },
+    2: {
+        question: "What is the maximum vertical g-force a human body can withstand before loss of consciousness?",
+        answer: "5-10 seconds at 4-5g",
+        options: {
+            a: "5-10 seconds at 2g",
+            b: "5-10 seconds at 45g",
+            c: "5-10 seconds at 4-5g",
+            d: "5-10 seconds at 15g",
         },
-        hint: "^[a-z]\s[A-Z]$"
-    }
+    },
+    3: {
+        question: "Which aircraft was the first tp receive type certification in USA?",
+        answer: "The Buhl Airster CA-3",
+        options: {
+            a: "The Boeing 707",
+            b: "The Buhl Airster CA-3",
+            c: "the BOAC Comet",
+            d: "The Wright flyer",
+        },
+    },
+    4: {
+        question: "The World’s first space telescope, Hubble, was launched in 1990, but when did it go offline?",
+        answer: "It is still working",
+        options: {
+            a: "1990",
+            b: "2010",
+            c: "2018",
+            d: "It is still working",
+        },
+    },
+    5: {
+        question: "A(n) ________ is the shape of an airplane’s wing.",
+        answer: "Airfoil",
+        options: {
+            a: "Flap",
+            b: "Yaw",
+            c: "Pitch",
+            d: "Airfoil",
+        },
+    },
+    6: {
+        question: "Side-to-side movement of the nose and tail of the aircraft.",
+        answer: "Yaw",
+        options: {
+            a: "Pitch",
+            b: "Thrust",
+            c: "Lift",
+            d: "Yaw",
+        },
+    },
+    7: {
+        question: "The International Space Station is an example of a ________.",
+        answer: "Satellite",
+        options: {
+            a: "Space Plane",
+            b: "Satellite",
+            c: "Rocket",
+            d: "Rover",
+        },
+    },
+    8: {
+        question: "______ help engineers study the effects of moving air.",
+        answer: "Wind tunnels",
+        options: {
+            a: "Large Vaccuums",
+            b: "Wind farms",
+            c: "Wind tunnels",
+            d: "Windmills",
+        },
+    },
+    9: {
+        question: "Avionics describe________",
+        answer: "aviation electronics",
+        options: {
+            a: "aviation electronics",
+            b: "wing electronics",
+            c: "landing gear",
+            d: "Navigation",
+        },
+    },
+    10: {
+        question: "_____ wings are used on most commercial airlines.",
+        answer: "Swept",
+        options: {
+            a: "Delta",
+            b: "Swept",
+            c: "Biplane",
+            d: "Glider",
+        },
+    },
+    11: {
+        question: "_________ engines introduce additional fuel into exhaust.",
+        answer: "Afterburning",
+        options: {
+            a: "Afterburning",
+            b: "Later burning",
+            c: "Jet fusion",
+            d: "Propulsion",
+        },
+    },
+    12: {
+        question: "Who coined the term avionics?",
+        answer: "Philip Klass",
+        options: {
+            a: "Philip Klass",
+            b: "Edwin Walter",
+            c: "Steve Roberts",
+            d: "Paul Carson",
+        },
+    },
+    13: {
+        question: "The _______ is the angle of the chord line of the wing relative to the air flow",
+        answer: "Angle of Attack",
+        options: {
+            a: "Stall angle",
+            b: "Fixed wing angle",
+            c: "Angle of Attack",
+            d: "Chord Angle",
+        },
+    },
+    14: {
+        question: "Hinged main surface part that help control banking for a turn.",
+        answer: "Aileron",
+        options: {
+            a: "Horizontal stabilizer",
+            b: "Rudder",
+            c: "Aileron",
+            d: "Pitch",
+        },
+    },
+    15: {
+        question: "The relationship between the length and width of a wing.",
+        answer: "Aspect Ratio",
+        options: {
+            a: "Aerodynamics",
+            b: "Apect Ratio",
+            c: "Static Stability",
+            d: "Flaps",
+        },
+    },
+    16: {
+        question: "Who was the first to invent autopilot?",
+        answer: "Lawrence Sperry",
+        options: {
+            a: "Jack Jaeckas",
+            b: "Johnson Reis",
+            c: "James Watkins",
+            d: "Lawrence Sperry",
+        },
+    },
 }
-var i=0;
+
+var i = 0;
 var ans = document.getElementById("answer");
-var points =0;
+var points = 0;
 var len = Object.keys(questions).length;
-var index = Math.floor(Math.random()*4 + 1);
-const inc  = Math.floor(Math.random()*2 + 1);
+var index = Math.floor(Math.random() * 4 + 1);
+const inc = Math.floor(Math.random() * 2 + 1);
 var prevIndex;
+let mcqI = 1;
+let mcqPoints = 0;
 genQues(index);
 
-function genQues(ind){
-    if(ind > len){
-        index = 0 ;
+function genQues(ind) {
+    if (ind > len) {
+        index = 0;
         ind = index + inc;
     }
     prevIndex = ind;
     index = ind;
     document.getElementById("question").innerHTML = JSON.stringify(questions[ind].question);
 }
-function genTimer(){
+
+function genTimer() {
     var timeleft = 60;
-    var Timer = setInterval(function(){
-      if(timeleft < 0){
-        console.log(points);
-        displayScore(points);
-        $('#endGame').modal('show');
-        clearInterval(Timer);
-      } else {
-        document.getElementById("countdown").innerHTML = timeleft;
-      }
-      timeleft--;
+    var Timer = setInterval(function () {
+        if (timeleft < 0) {
+            setTimeout(() => {
+                document.querySelector(".element2").scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }, 800);
+            mcqTimer();
+            genMCQ(mcqI);
+            clearInterval(Timer);
+        } else {
+            document.getElementById("countdown").innerHTML = timeleft;
+        }
+        timeleft--;
     }, 1000);
 }
-function displayScore(points){
-    var point = "\xa0"+ points.toString(10)  + "\xa0";
+
+function displayScore(points) {
+    var point = "\xa0" + points.toString(10) + "\xa0";
     document.getElementById("points").innerHTML = point;
-    $('[id="submit"]').prop('onclick',null).off('click');
-    $('[id="hint"]').prop('onclick',null).off('click');
-    $('[id="ans"]').attr('disabled',"disabled");
+    $('[id="submit"]').prop('onclick', null).off('click');
+    $('[id="hint"]').prop('onclick', null).off('click');
+    $('[id="ans"]').attr('disabled', "disabled");
     document.getElementById("question").innerHTML = "You've already attempted it.";
 }
-function printHint(){
+
+function printHint() {
     document.getElementById("hintText").innerHTML = JSON.stringify(questions[prevIndex].hint);
 }
-function nextQues(){
+
+function nextQues() {
     let ans = document.getElementById("ans").value;
     document.getElementById("ans").value = "";
     document.getElementById("hintText").innerHTML = "";
-    if(ans === null){
+    if (ans === null) {
         genQues(index + inc);
     }
-    if(ans.toLowerCase() == questions[prevIndex].answer.toLowerCase()){
+    if (ans.toLowerCase() == questions[prevIndex].answer.toLowerCase()) {
         points++;
         document.getElementById("scoreVal").innerHTML = points.toString(10);
     }
-    genQues(index+inc);
+    genQues(index + inc);
+}
+
+function mcqTimer(){
+    var timeleft = 60;
+    var Timer = setInterval(function () {
+        if (timeleft < 0 || mcqI > 15) {
+            console.log(points + mcqPoints);
+            displayScore(points + mcqPoints);
+            $('#endGame').modal('show');
+            clearInterval(Timer);
+        } else {
+            document.getElementById("mcqTimer").innerHTML = timeleft;
+        }
+        timeleft--;
+    }, 1000);
+}
+
+function genMCQ(mcqIndex){
+    document.querySelector(".mcqQuestion").innerHTML = mcqQues[mcqIndex].question
+    document.querySelector("#a").innerHTML = mcqQues[mcqIndex].options['a']
+    document.querySelector("#b").innerHTML = mcqQues[mcqIndex].options['b']
+    document.querySelector("#c").innerHTML = mcqQues[mcqIndex].options['c']
+    document.querySelector("#d").innerHTML = mcqQues[mcqIndex].options['d']
+}
+
+function clearScr(){
+    document.querySelector(".mcqQuestion").innerHTML = ""
+    document.querySelector("#a").innerHTML = ""
+    document.querySelector("#b").innerHTML = ""
+    document.querySelector("#c").innerHTML = ""
+    document.querySelector("#d").innerHTML = ""
+}
+
+function checkAnsMCQ(option){
+    if(mcqQues[mcqI].answer === mcqQues[mcqI].options[option]){
+        mcqPoints++;
+        document.querySelector("#mcqscoreVal").innerHTML = mcqPoints
+    }
+    clearScr();
+    genMCQ(++mcqI);
 }
